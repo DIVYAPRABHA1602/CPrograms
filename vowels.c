02b6c94e80afc34dd9115e180e7f7bf61eb0e3e5
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-07-06 12:01:26 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("\n enter the char");
+scanf("%d",&c);
+switch(ch)
+{
+case 'a:
+printf("|n vowel");
+case 'e':
+printf("\n vowel");
+case 'i':
+printf("\n vowel");
+case 'o':
+printf("\n vowel");
+case 'u':
+printf("\n vowel");
+default:
+printf("\n not a vowel ,its a charcter");
+}
