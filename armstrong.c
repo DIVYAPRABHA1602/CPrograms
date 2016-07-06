b1d5afac1c42d03fccaff60fcc5d4be2ed80ab63
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-07-06 14:43:25 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rem,num;
+printf("\n enterr number");
+scanf("%d",&n);
+num=1;
+rem=n%10;
+num=(num*num*num)+rem*10;
+n=n/10;
+printf("%d",num);
+
