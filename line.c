51118d795ee1ea78a6f69be185e5bac693d59fbc
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-07-15 21:15:36 +0530

    Create line.c

---
+++ line.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int i,ch=0,w=1,l=1;
+char str[100];
+printf("\n enter the string");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+ch=ch+1;
+}
+for(i=0;i=ch-1;i++)
+{
+w=w+1;
+}
+for(i=0;str[i]!='\n';i++)
+{
+l=l+1;
+}
+printf("\n no of lines %d",l);
+}
