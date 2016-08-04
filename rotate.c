9ca367a3c9e6828ab50ecb15cd9e726a78b094e9
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-08-04 22:25:53 +0530

    Create rotate.c

---
+++ rotate.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int i,a[5],n;
+printf("\n numbers");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("d");
+scanf("%d",&d);
+for(i=0;i<n'i++)
+{
+a[i]=a[d];
+a[i+1]=a[d+1];
+}
+for(i-=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
